b4078f168f1dc8e34301865026cf29946d68ca7e
Ammar-Mohamed-Elz3eeem <ammarmohamedthez@gmail.com>
2023-03-24 22:40:48 +0200

    add configuration for school file

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+# create a file named scool on tmp folder
+
+file { '/tmp/school':
+  ensure  => present,
+  mode    => '0744',
+  group   => 'www-data',
+  owner   => 'www-data',
+  content => 'I love Puppet'
+}
