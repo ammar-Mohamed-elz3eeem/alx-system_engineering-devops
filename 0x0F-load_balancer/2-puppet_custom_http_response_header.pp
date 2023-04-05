bda89824e61c25d69e62c168c10bb61f844a94a7
Ammar-Mohamed-Elz3eeem <ammarmohamedthez@gmail.com>
2023-04-05 03:17:07 +0200

    add CM for adding custom header

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,30 @@
+# - setup New Ubuntu server with nginx
+# - add a custom HTTP header
+
+exec { 'update system':
+  command => '/usr/bin/apt-get update'
+}
+
+package { 'nginx':
+  ensure  => 'installed',
+  require => Exec['update system']
+}
+
+file { '/var/www/html/index.html':
+  content => 'Hello World!'
+}
+
+exec { 'redirect_me command':
+  command  => 'sed -i "24i\    rewrite ^/redirect_me$ https://www.youtube.com/ permanent;" /etc/nginx/sites-available/default',
+  provider => 'shell'
+}
+
+exec { 'redirect_me command':
+  command  => 'sed -i "25i\    add_header X-Served-By \$hostname;" /etc/nginx/sites-available/default',
+  provider => 'shell'
+}
+
+package { 'nginx':
+  ensure  => running,
+  require => package['nginx']
+}
