b9c1763a1c53464a76db9b536e7f40d41a46e7fe
Ammar-Mohamed-Elz3eeem <ammarmohamedthez@gmail.com>
2023-04-24 22:54:13 +0200

    update saved to csv module

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -3,18 +3,18 @@
 module to save employees tasks in .csv file format
 """
 import csv
-import json
-from requests import get
-from sys import argv
-
+import requests
+import sys
 
 if __name__ == "__main__":
-    empid = int(argv[1])
+    user_id = sys.argv[1]
     url = "https://jsonplaceholder.typicode.com/"
-    empinfo = get("{}users/{}".format(url, empid)).json()
-    emptodos = get("{}todos".format(url), {"userId":empid}).json()
-    with open("{}.csv".format(empid), "w", newline="") as fd:
-        writer = csv.writer(fd, quoting=csv.QUOTE_ALL)
-        [writer.writerow([row.get("userId"),
-                          empinfo.get("username"), row.get("completed"), row.get("title")])
-         for row in emptodos]
+    user = requests.get(url + "users/{}".format(user_id)).json()
+    username = user.get("username")
+    todos = requests.get(url + "todos", params={"userId": user_id}).json()
+
+    with open("{}.csv".format(user_id), "w", newline="") as csvfile:
+        writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
+        [writer.writerow(
+            [user_id, username, t.get("completed"), t.get("title")]
+         ) for t in todos]
