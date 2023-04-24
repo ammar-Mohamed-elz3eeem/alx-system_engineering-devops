54474df265b896bf3496c2fcc549aa43251660f7
Ammar-Mohamed-Elz3eeem <ammarmohamedthez@gmail.com>
2023-04-24 21:23:37 +0200

    update format

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -35,6 +35,6 @@ if __name__ == "__main__":
         .format(
             x["userId"],
             empinfo["username"],
-            str(x["completed"]).capitalize(),
+            str(x["completed"]),
             x["title"]
         )), emptodos))
