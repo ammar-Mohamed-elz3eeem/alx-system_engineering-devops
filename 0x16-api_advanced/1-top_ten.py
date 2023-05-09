148c8fe10e9bcad28ae5cb72d56a9e22116781f6
Ammar-Mohamed-Elz3eeem <ammarmohamedthez@gmail.com>
2023-05-09 15:12:00 +0200

    check response code

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -6,12 +6,12 @@ import requests
 
 def top_ten(subreddit):
     """return 10 hot posts in subreddit"""
-    if subreddit is None or type(subreddit) is not str:
+    url = "https://www.reddit.com"
+    res = requests\
+        .get("{}/r/{}/hot.json".format(url, subreddit), {"limit": 10},
+             headers={"User-Agent": "Ammar"}, allow_redirects=False)
+    if res.status_code is not 200:
         print(None)
         return
-    url = "https://www.reddit.com"
-    posts = requests\
-        .get("{}/r/{}/hot.json".format(url, subreddit), {"limit": 10},
-             headers={"User-Agent": "Ammar"}, allow_redirects=False)\
-        .json().get("data", {}).get("children", [])
+    posts = res.json().get("data", {}).get("children", [])
     [print(post.get("data", {}).get("title")) for post in posts]
