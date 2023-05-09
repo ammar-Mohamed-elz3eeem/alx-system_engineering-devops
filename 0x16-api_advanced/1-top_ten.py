f9dc077cb50d73eadbff74d1cfbcc26b68cd294f
Ammar-Mohamed-Elz3eeem <ammarmohamedthez@gmail.com>
2023-05-09 15:00:39 +0200

    add README + getting hot posts and subscribers on subreddit

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Queries the Reddit API and returns
+the hottest ten posts in subreddit
+"""
+import requests
+
+
+def top_ten(subreddit):
+    if subreddit is None or type(subreddit) is not str:
+        print(None)
+        return
+    url = "https://www.reddit.com"
+    posts = requests\
+        .get("{}/r/{}/hot.json".format(url, subreddit), {"limit": 10},
+             headers={"User-Agent": "Ammar"})\
+        .json().get("data", {}).get("children", [])
+    [print(post.get("data", {}).get("title")) for post in posts]
