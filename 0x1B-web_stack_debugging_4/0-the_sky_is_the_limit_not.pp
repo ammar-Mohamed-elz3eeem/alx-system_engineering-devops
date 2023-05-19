6d968b97873328eb511671bb4f7078672cc61cf1
Ammar-Mohamed-Elz3eeem <ammarmohamedthez@gmail.com>
2023-05-19 23:28:56 +0200

    rewrite the file

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,11 +1,13 @@
-# fix nginx request limit
+# increases the amount of traffic an Nginx can recieve.
 
-exec { 'Fix Nginx':
+# fix nginx request limit
+exec { 'fix: for nginx':
   command => 'sed -i "s/15/4096/" /etc/default/nginx',
   path    => '/usr/local/bin/:/bin/'
-}
+} ->
 
-exec { 'restart nginx':
-  command  => 'nginx restart',
-  path     => '/etc/init.d/'
+# Restart Nginx
+exec { 'restart: nginx-restart':
+  command => 'nginx restart',
+  path    => '/etc/init.d/'
 }
