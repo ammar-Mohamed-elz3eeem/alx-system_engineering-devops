f4665e0013901dd518bface3265bb157285f8071
Ammar-Mohamed-Elz3eeem <ammarmohamedthez@gmail.com>
2023-05-19 23:06:24 +0200

    fix puppet-lint errors

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -2,7 +2,7 @@
 
 exec { 'Fix Nginx':
   command => "sed -i 's/15/4096/' /etc/default/nginx",
-  path    => ["/bin", "/usr/local/bin"]
+  path    => ['/bin', '/usr/local/bin']
 }
 
 service { 'nginx':
