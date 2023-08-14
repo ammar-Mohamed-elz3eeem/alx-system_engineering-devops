02d8bf841884b9d62ad3d9b9b7e26e79fcd6d4d8
Ammar-Mohamed-Elz3eeem <ammarmohamedthez@gmail.com>
2023-08-14 12:49:13 +0200

    add number_of_subscribbers

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,18 @@
+"""
+Get SubReddit info
+"""
+import requests
+import sys
+
+
+def number_of_subscribers(subreddit):
+    """
+    query reddit website to get number of subscribers
+    to sub redit
+    """
+    if subreddit is None or type(subreddit) is not str:
+        return 0
+    headers = {"User-Agent": "Ammar"}
+    url = "http://www.reddit.com/r/{}/about.json".format(subreddit)
+    data = requests.get(url, headers=headers)
+    return data.json().get("data", {}).get("subscribers", 0)
