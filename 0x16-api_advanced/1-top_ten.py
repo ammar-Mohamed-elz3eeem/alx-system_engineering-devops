446b7454c596699f68a9827c6d23dc1bea10f82c
Ammar-Mohamed-Elz3eeem <ammarmohamedthez@gmail.com>
2023-08-14 13:23:06 +0200

    add subreddit top ten functionality

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,23 @@
+"""
+Get top ten posts in subreddit
+"""
+import requests
+
+
+def top_ten(subreddit):
+    """
+    query reddit website to get top ten posts in subreddit
+    """
+    if subreddit is None or type(subreddit) is not str:
+        return None
+    headers = {"User-Agent": "Ammar"}
+    params = {"limit": 10}
+    url = "http://www.reddit.com/r/{}/hot/.json".format(subreddit)
+    res = requests.get(url, headers=headers, params=params,
+                       allow_redirects=False)
+    if (res.status_code == 404):
+        print("None")
+        return
+    data = res.json().get("data", {}).get("children", [])
+    mapped_data = map(lambda data: data.get("data", {}).get("title", ""), data)
+    [print(row) for row in list(mapped_data)]
