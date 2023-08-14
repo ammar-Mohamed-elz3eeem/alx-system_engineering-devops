00989ddcb412268b6fc4360af41440087d51c973
Ammar-Mohamed-Elz3eeem <ammarmohamedthez@gmail.com>
2023-08-14 14:40:11 +0200

    fix error code

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -9,16 +9,13 @@ def top_ten(subreddit):
     """
     query reddit website to get top ten posts in subreddit
     """
-    if subreddit is None or type(subreddit) is not str:
-        return None
     headers = {"User-Agent": "Ammar"}
     params = {"limit": 10}
-    url = "http://www.reddit.com/r/{}/hot/.json".format(subreddit)
+    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
     res = requests.get(url, headers=headers, params=params,
                        allow_redirects=False)
     if (res.status_code == 404):
         print("None")
         return
     data = res.json().get("data").get("children")
-    mapped_data = map(lambda row: row.get("data").get("title"), data)
-    [print(row) for row in list(mapped_data)]
+    [print(row.get("data").get("title")) for row in data]
