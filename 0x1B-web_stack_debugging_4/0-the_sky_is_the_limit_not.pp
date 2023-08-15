04a99c50b98663642e697665f38a8130c1e7f1fe
Ammar-Mohamed-Elz3eeem <ammarmohamedthez@gmail.com>
2023-08-15 21:31:25 +0200

    add script to increase server traffix

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,11 @@
+# let server handle big traffic
+
+exec { 'fix nginx config'
+  command => 'sed -i "s/15/4096/" /etc/default/nginx'
+  path    => '/usr/local/bin/:/bin/'
+} ->
+
+exec { 'restart nginx'
+  command => 'nginx restart',
+  path    => '/etc/init.d'
+}
