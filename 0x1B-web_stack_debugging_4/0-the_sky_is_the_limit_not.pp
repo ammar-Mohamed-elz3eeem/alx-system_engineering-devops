e2143d4ee3df60ea3c125b0e06294eb5436d56de
Ammar-Mohamed-Elz3eeem <ammarmohamedthez@gmail.com>
2023-08-18 05:46:20 +0200

    fix puppet lint

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,11 +1,9 @@
 # let server handle big traffic
 
-exec { 'fix nginx config'
+exec { 'fix--for-nginx':
   command => 'sed -i "s/15/4096/" /etc/default/nginx',
   path    => '/usr/local/bin/:/bin/'
-} ->
-
-exec { 'restart nginx'
+} -> exec { 'nginx-restart':
   command => 'nginx restart',
-  path    => '/etc/init.d'
+  path    => '/etc/init.d/'
 }
