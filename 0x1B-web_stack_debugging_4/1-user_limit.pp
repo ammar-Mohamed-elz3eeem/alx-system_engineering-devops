48a0a669dc2734569d87887ca79269698fd5dac3
Ammar-Mohamed-Elz3eeem <ammarmohamedthez@gmail.com>
2023-08-18 05:49:17 +0200

    edit limits file name

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -2,11 +2,11 @@
 # open hard & soft files without errors
 
 exec {'enable for hard file':
-  command => 'sed -i "/holberton hard/s/5/50000/" /etc/security/limit.conf',
+  command => 'sed -i "/holberton hard/s/5/50000/" /etc/security/limits.conf',
   path    => '/usr/local/bin/:/bin/'
 }
 
 exec {'enable for soft file':
-  command => 'sed -i "/holberton soft/s/4/50000/" /etc/security/limit.conf',
+  command => 'sed -i "/holberton soft/s/4/50000/" /etc/security/limits.conf',
   path    => '/usr/local/bin/:/bin/'
 }
